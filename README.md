a84e4d233b1a17655df9ee415f32b1aba9f7279b
th2nhb1nh <thanhbinh.pham610@gmail.com>
2024-06-19 17:27:57 +0700

    add the part to install the headline theme, which I prefer ✨

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # zsh-autosetup
 
+Auto-setup zsh, oh-my-zsh, and some plugins I find useful.
+
 ```bash
 git clone https://github.com/th2nhb1nh/zsh-autosetup.git
 cd zsh-autosetup
--- zsh-autosetup.sh
+++ zsh-autosetup.sh
@@ -10,6 +10,8 @@ chsh -s $(which zsh)
 echo $SHELL
 
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+# Clone the plugins
 cd $ZSH_CUSTOM/plugins
 repos=(
     "https://github.com/zshzoo/cd-ls"
@@ -26,7 +28,13 @@ for repo in "${repos[@]}"; do
     git clone $repo
 done
 
+# Clone the theme
+git clone https://github.com/moarram/headline.git $ZSH_CUSTOM/themes/headline
+ln -s $ZSH_CUSTOM/themes/headline/headline.zsh-theme $ZSH_CUSTOM/themes/headline.zsh-theme
+
 sed -i 's/plugins=(git)/plugins=(git cd-ls zsh-autosuggestions zsh-syntax-highlighting colorize history-search-multi-word zsh-plugin-reload cmdtime auto-notify)/' ~/.zshrc
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="headline"/' ~/.zshrc
+
 source ~/.zshrc
 
 echo "Zsh, Oh My Zsh, and plugins installation complete. Please restart your terminal."
